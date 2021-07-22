d261e75accdc31e3243f665b75301ea11eb1509b
Bit0r <nie_wang@outlook.com>
2021-07-22 12:14:10 +0800

    first commit

---
+++ labs/lab03.py
@@ -0,0 +1,67 @@
+HW_SOURCE_FILE = __file__
+
+
+def summation(n, term):
+    """Return the sum of numbers 1 through n (including n) wíth term applied to each number.
+    Implement using recursion!
+
+    >>> summation(5, lambda x: x * x * x) # 1^3 + 2^3 + 3^3 + 4^3 + 5^3
+    225
+    >>> summation(9, lambda x: x + 1) # 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+    54
+    >>> summation(5, lambda x: 2**x) # 2^1 + 2^2 + 2^3 + 2^4 + 2^5
+    62
+    >>> # Do not use while/for loops!
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'summation',
+    ...       ['While', 'For'])
+    True
+    """
+    assert n >= 1
+
+    if n == 1:
+        return term(1)
+
+    return summation(n - 1, term) + term(n)
+
+
+def pascal(row, column):
+    """Returns the value of the item in Pascal's Triangle
+    whose position is specified by row and column.
+    >>> pascal(0, 0)
+    1
+    >>> pascal(0, 5)	# Empty entry; outside of Pascal's Triangle
+    0
+    >>> pascal(3, 2)	# Row 3 (1 3 3 1), Column 2
+    3
+    >>> pascal(4, 2)     # Row 4 (1 4 6 4 1), Column 2
+    6
+    """
+    row_old, row_new = [0] * 1024, [0] * 1024
+
+    row_new[1] = 1
+    for i in range(2, row + 2):
+        row_old, row_new = row_new, row_old
+        for j in range(1, i + 1):
+            row_new[j] = row_old[j - 1] + row_old[j]
+
+    return row_new[column + 1]
+
+
+def paths(m, n):
+    """Return the number of paths from one corner of an
+    M by N grid to the opposite corner.
+
+    >>> paths(2, 2)
+    2
+    >>> paths(5, 7)
+    210
+    >>> paths(117, 1)
+    1
+    >>> paths(1, 157)
+    1
+    """
+    if m == 1 or n == 1:
+        return 1
+    return paths(m - 1, n) + paths(m, n - 1)
